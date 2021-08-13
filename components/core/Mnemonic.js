e4e1f4f7761996312a5e896f54de550f587b46ca
Peibin Zhang <ZhangPeibin@users.noreply.github.com>
2021-08-13 15:07:10 +0800

    修改auth

--- components/core/Mnemonic.js
+++ components/core/Mnemonic.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 // 助记词登录组件
 /** @jsx jsx */
 import {jsx, css} from "@emotion/react";
@@ -8,7 +8,7 @@ import * as SVG from "../../common/svg";
 import * as Styles from "../../common/styles";
 import * as Constants from "../../common/constants";
 import {BackgroundGenerator} from "../../pages/user/auth";
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
 import * as R from "@common/requests";
 
 
@@ -33,14 +33,14 @@ const MnemoincExist = () => {
     const router = useRouter();
 
     let mnemonic = "";
-    const onInputChange = (target)=>{
-        mnemonic =  target.currentTarget.value;
+    const onInputChange = (target) => {
+        mnemonic = target.currentTarget.value;
     }
 
     const onPrivateKey = async () => {
         const identity = await R.onRegenerateIdentityByMnemonic(mnemonic)
-        localStorage.setItem("identity",identity)
-        await router.replace({pathname:"/dashboard"})
+        localStorage.setItem("identity", identity)
+        await router.replace({pathname: "/dashboard"})
     }
 
     // @ts-ignore
@@ -57,11 +57,13 @@ const MnemoincExist = () => {
                     onChange={onInputChange}
                     className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
                     placeholder="recovery phrase"
-                    style={{transition: "all .15s ease",  borderColor: "#FF715E",
-                        border:"none",
-                        overflow:"hidden",
-                        resize:"none",
-                        height:"5rem",}}
+                    style={{
+                        transition: "all .15s ease", borderColor: "#FF715E",
+                        border: "none",
+                        overflow: "hidden",
+                        resize: "none",
+                        height: "5rem",
+                    }}
                 />
             </div>
 
@@ -70,13 +72,13 @@ const MnemoincExist = () => {
                     onClick={onPrivateKey}
                     className="bg-orange-600 text-white active:bg-orange-200 text-sm font-bold  px-6 py-3 rounded-md shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
                     type="button"
-                    style={{transition: "all .15s ease",backgroundColor: "#FF715E"}}
+                    style={{transition: "all .15s ease", backgroundColor: "#FF715E"}}
                 >
                     Next
                 </button>
             </div>
         </div>
-        );
+    );
 }
 
 const MnemoincNotExist = ({mnemoin}) => {
@@ -91,11 +93,11 @@ const MnemoincNotExist = ({mnemoin}) => {
         await R.onRegenerateMnemonic(state, setState);
     }
 
-    const next = async () =>{
+    const next = async () => {
         const mnemonic = state.mnemonic;
         const identity = await R.onRegenerateIdentityByMnemonic(mnemonic)
-        localStorage.setItem("identity",identity)
-        await router.replace({pathname:"/dashboard"})
+        localStorage.setItem("identity", identity)
+        await router.replace({pathname: "/dashboard"})
     }
 
     return (
@@ -107,16 +109,16 @@ const MnemoincNotExist = ({mnemoin}) => {
 
             <div
                 style={{
-                    display:state.mnemonic==null?"none":"block",
-                    backgroundColor:"#fff",
-                    marginBottom:"0.3rem",
-                    marginTop:"0.4rem",
-                    paddingLeft:"0.6rem",
-                    paddingRight:"0.4rem",
-                    paddingTop:"0.8rem",
-                    paddingBottom:"0.8rem",
+                    display: state.mnemonic == null ? "none" : "block",
+                    backgroundColor: "#fff",
+                    marginBottom: "0.3rem",
+                    marginTop: "0.4rem",
+                    paddingLeft: "0.6rem",
+                    paddingRight: "0.4rem",
+                    paddingTop: "0.8rem",
+                    paddingBottom: "0.8rem",
                     borderRadius: "0.25rem",
-                    boxShadow:"0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)"
+                    boxShadow: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)"
                 }}>
                 <p>
                     {mnemoin || state.mnemonic}
@@ -136,8 +138,9 @@ const MnemoincNotExist = ({mnemoin}) => {
                 onClick={next}
                 className="mt-2 bg-orange-600 text-white active:bg-orange-200 text-sm font-bold  px-6 py-3 rounded-md shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
                 type="button"
-                style={{transition: "all .15s ease",
-                    display:state.mnemonic==null?"none":"block",
+                style={{
+                    transition: "all .15s ease",
+                    display: state.mnemonic == null ? "none" : "block",
                     backgroundColor: "#FF715E"
                 }}
             >
@@ -151,7 +154,7 @@ const MnemoincNotExist = ({mnemoin}) => {
                 style={{margin: "0px auto", marginTop: "20px"}}
             /> */}
         </div>
-        );
+    );
 }
 
 // This function gets called at build time on server-side.
@@ -174,59 +177,59 @@ export async function getStaticProps() {
 
 
 export default class Mnemonic extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
     render() {
         return (
             <>
-            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-200 border-0">
-                <H3 style={{
+                <div
+                    className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-200 border-0">
+                    <H3 style={{
                         textAlign: "center",
-                        lineHeight: "30px",
                         padding: "36px 32px  24px 32px",
                     }}
-                >
-                    {"Use wallet's recovery phrase for security"}
-                </H3>
+                    >
+                        {"Use wallet's recovery phrase for security"}
+                    </H3>
+                    <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                        <MnemoincNotExist/>
+                        <div className="text-gray-500 text-center mb-3 font-bold">
+                            <small>Or I already have an account mnemonic.</small>
+                        </div>
 
-                <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                    <MnemoincNotExist/>
-                    <div className="text-gray-500 text-center mb-3 font-bold">
-                        <small>Or I already have an account mnemonic.</small>
-                    </div>
+                        <MnemoincExist/>
 
-                    <MnemoincExist/>
-
-                    <div className="mt-28">
-                        <div style={{marginTop: "auto"}}>
-                            <div css={STYLES_LINK_ITEM}>
-                                <div css={Styles.HORIZONTAL_CONTAINER_CENTERED} onClick={()=>this.props.back()}>
-                                    <SVG.ArrowDownLeft height="16px" style={{marginRight: 4}}/> Back
+                        <div className="mt-28">
+                            <div style={{marginTop: "auto"}}>
+                                <div css={STYLES_LINK_ITEM}>
+                                    <div css={Styles.HORIZONTAL_CONTAINER_CENTERED} onClick={() => this.props.back()}>
+                                        <SVG.ArrowDownLeft height="16px" style={{marginRight: 4}}/> Back
+                                    </div>
                                 </div>
+
+                                <a css={STYLES_LINK_ITEM}
+                                   href="https://support.token.im/hc/en-us/articles/360002074233-What-is-Mnemonic-Phrase"
+                                   target="_blank">
+                                    <div css={Styles.HORIZONTAL_CONTAINER_CENTERED}>
+                                        <SVG.RightArrow height="16px" style={{marginRight: 4}}/> What is Wallet Recovery
+                                        Phrase
+                                    </div>
+                                </a>
+
+                                <a css={STYLES_LINK_ITEM} style={{marginTop: 4}}
+                                   href="https://ipfser.org/2019/11/03/ipfsshengtaibaogao%e4%b8%a8an-overview-of-distributed-storage-ipfs/"
+                                   target="_blank">
+                                    <div css={Styles.HORIZONTAL_CONTAINER_CENTERED}>
+                                        <SVG.RightArrow height="16px" style={{marginRight: 4}}/> What is IPFS
+                                    </div>
+                                </a>
                             </div>
-
-                            <a css={STYLES_LINK_ITEM}
-                                href="https://support.token.im/hc/en-us/articles/360002074233-What-is-Mnemonic-Phrase"
-                                target="_blank">
-                                <div css={Styles.HORIZONTAL_CONTAINER_CENTERED}>
-                                    <SVG.RightArrow height="16px" style={{marginRight: 4}}/> What is Wallet Recovery Phrase
-                                </div>
-                            </a>
-
-                            <a css={STYLES_LINK_ITEM} style={{marginTop: 4}}
-                                href="https://ipfser.org/2019/11/03/ipfsshengtaibaogao%e4%b8%a8an-overview-of-distributed-storage-ipfs/"
-                                target="_blank">
-                                <div css={Styles.HORIZONTAL_CONTAINER_CENTERED}>
-                                    <SVG.RightArrow height="16px" style={{marginRight: 4}}/> What is IPFS
-                                </div>
-                            </a>
                         </div>
                     </div>
                 </div>
-            </div>    
-        </>
+            </>
         )
     }
 }
